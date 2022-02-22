04e34e185d5d6813846727d7dee6752c59d5020b
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2022-02-22 21:52:46 +0000

    base

--- app2.py
+++ app2.py
@@ -1,14 +1,22 @@
 # realizzare server web che visualizzi l'orario e colori lo sfondo in base all'orario 
 from flask import Flask, render_template
 from datetime import datetime
+
 app = Flask(__name__)
 
-print()
+@app.route("/",methods=["GET"])
+def time():
+  hour = datetime.now().hour
+  if hour < 7:
+    return render_template("index.html", color = "DarkBlue", testo = "É notte, sono le " + str(hour))
+  elif hour < 13:
+    return render_template("index.html", color = "red", testo = "É mattina, sono le " + str(hour))
+  elif hour < 18:
+    return render_template("index.html", color = "orange", testo = "É pomeriggio, sono le " + str(hour))
+  else:
+    return render_template("index.html", color = "blue", testo = "É sera, sono le " + str(hour))
 
 
-
-
- 
 
 
 
