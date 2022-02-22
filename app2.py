d8035462c019adc8a7d8653d141d8ad97fbdcd34
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2022-02-22 12:58:54 +0000

    base

---
+++ app2.py
@@ -0,0 +1,16 @@
+# realizzare server web che visualizzi l'orario e colori lo sfondo in base all'orario 
+from flask import Flask, render_template
+from datetime import datetime
+app = Flask(__name__)
+
+print()
+
+
+
+
+ 
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
