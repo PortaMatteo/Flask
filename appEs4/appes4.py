f477ad73a31c4d8bde77b06efdabfcec3dc84e1f
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2022-03-15 10:58:09 +0000

    gh

---
+++ appEs4/appes4.py
@@ -0,0 +1,20 @@
+# Si vuole realizzare un sito web che permetta di visualizzare alcune informazioni sull'andamento dell'epidemia di covid 
+# nel nostro paese a partire dai dati presenti nel file https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/platea-dose-addizionale-booster.csv
+# L'utente sceglie la regione da un elenco (menù a tendina), clicca su un bottone e il sito deve visualizzare una tabella
+# Contente le informazioni relative a quella regione 
+# I dati da inserire nel menù a tendina devono essere caricati automaticamente dalla pagina 
+
+from flask import Flask, render_template, request 
+app = Flask(__name__)
+import pandas as pd 
+
+df = pd.read_csv("https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/platea-dose-addizionale-booster.csv")
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("homees4.html")
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
