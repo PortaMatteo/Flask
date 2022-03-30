a5670961b9d8d55a34ba7e03c59243ac9a678eea
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2022-03-30 12:54:41 +0000

    base

---
+++ CorrezioneVerificaA/verificaA.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, Response
+app = Flask(__name__)
+
+import pandas as pd
+
+stazioni = pd.read_csv("/workspace/Flask/CorrezioneVerificaA/static/files/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv", sep=";")
+
+@app.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
+
+@app.route("/numero", methods=["GET"])
+def numero():
+    #numero stazioni per ogni municipio
+    risultato = stazioni.groupby("MUNICIPIO")["OPERATORE"].count().reset_index()
+    return render_template("link1.html", risultato = risultato.to_html())
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
