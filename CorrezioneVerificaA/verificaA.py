0ea4ff4fa96b19a5a62465231eafd87471622d08
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2022-03-31 09:50:31 +0000

    base

--- CorrezioneVerificaA/verificaA.py
+++ CorrezioneVerificaA/verificaA.py
@@ -1,21 +1,52 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, redirect, url_for
 app = Flask(__name__)
 
+import io
 import pandas as pd
+import contextily
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 stazioni = pd.read_csv("/workspace/Flask/CorrezioneVerificaA/static/files/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv", sep=";")
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template("home.html")
+    return render_template("home1.html")
 
 @app.route("/numero", methods=["GET"])
 def numero():
+    global risultato
     #numero stazioni per ogni municipio
     risultato = stazioni.groupby("MUNICIPIO")["OPERATORE"].count().reset_index()
     return render_template("link1.html", risultato = risultato.to_html())
 
+@app.route("/grafico", methods=["GET"])
+def grafico():
+    #costruzione del grafico
+    fig, ax = plt.subplots(figsize = (6,4))
 
+    x = risultato.MUNICIPIO
+    y = risultato.OPERATORE
+
+    ax.bar(x, y)
+
+    #visualizzazione del grafico
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route("/selezione", methods=["GET"])
+def selezione():
+    scelta = request.args["scelta"]
+    if scelta == "es1":
+        return redirect(url_for("numero"))
+    elif scelta == "es2":
+        return redirect(url_for("input"))
+    else:
+        return redirect(url_for("dropdown"))
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
