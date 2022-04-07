b3056faa8550cd3b13d6124e1661333c857498f1
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2022-04-07 09:43:07 +0000

    base

--- CorrezioneVerificaC/verificaC.py
+++ CorrezioneVerificaC/verificaC.py
@@ -13,6 +13,8 @@ import matplotlib.pyplot as plt
 
 quartieri = gpd.read_file("/workspace/Flask/appEs6/static/files/ds964_nil_wm.zip")
 linee = gpd.read_file("https://dati.comune.milano.it/dataset/8bfe2015-2669-4796-9940-36b3c155b258/resource/b5acc06f-65fb-4481-9428-347fd1c18096/download/tpl_percorsi.geojson")
+ 
+linee["linea"] = linee["linea"].astype(int)
 
 @app.route("/", methods=["GET"])
 def home():
@@ -38,6 +40,40 @@ def elenco():
     Max = max(request.args["val1"], request.args["val2"])
     linee_distanza = linee[(linee["lung_km"] > Min) & (linee["lung_km"] < Max)].sort_values("linea")
     return render_template("elenco.html", tabella = linee_distanza.to_html())
+
+@app.route("/ricerca", methods=["GET"])
+def ricerca():
+    return render_template("ricerca.html")
+
+@app.route("/lineequart", methods=["GET"])
+def lineequart():
+    quartiere = request.args["quartiere"]
+    quartiereUtente = quartieri[quartieri["NIL"].str.contains(quartiere)]
+    linee_quartiere = linee[linee.intersects(quartiereUtente.geometry.squeeze())].sort_values("linea")
+    return render_template("lineequart.html", tabella = linee_quartiere.to_html())
+
+@app.route("/mappa", methods=["GET"])
+def mappa():
+    return render_template("tendina.html", linee = linee["linea"].drop_duplicates().sort_values(ascending=True))
+
+@app.route("/linea", methods=["GET"])
+def linea():
+    global lineeUtente
+    linea = int(request.args["linea"])
+    lineeUtente = linee[linee["linea"] == linea]
+    return render_template("mappa.html", linea = linea)
+
+@app.route("/mappa.png", methods=["GET"])
+def mappapng():
+    fig, ax = plt.subplots(figsize = (12,8))
+
+    lineeUtente.to_crs(epsg=3857).plot(ax=ax, edgecolor="k")
+    quartieri.to_crs(epsg=3857).plot(ax=ax, alpha=0.5, edgecolor="k")
+    contextily.add_basemap(ax=ax)   
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
